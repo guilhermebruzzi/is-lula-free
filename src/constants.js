25fd78270e63b252ece92ce2a7a0dc1958347595
Guilherme Bruzzi <guibruzzi@gmail.com>
2018-07-08 19:37:45 -0300

    Better message

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
 export const IS_LULA_FREE = false
-export const IS_LULA_FREE_QUESTION = 'O LULA ESTÁ LIVRE?'
+export const IS_LULA_FREE_QUESTION = 'LULA TÁ LIVRE?'
 export const LULA_FREE_MESSAGE = 'SIM'
 export const LULA_NOT_FREE_MESSAGE = 'NÃO'
